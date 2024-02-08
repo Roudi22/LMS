28352b08483c1f46d28e7d59b4fe7bb20ccf3f65
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-09 00:29:57 +0100

    add routes

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { editCourse, uploadCourse } from "../controllers/course.controller";
+import { addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
 const courseRouter = express.Router();
 
 courseRouter.post("/create-course",isAutheticated,authorizeRoles("admin"), uploadCourse);
 courseRouter.put("/edit-course/:id",isAutheticated,authorizeRoles("admin"), editCourse);
+courseRouter.get("/get-course/:id", getSingleCourse);
+courseRouter.get("/get-courses", getAllCourses);
+courseRouter.get("/access-course/:id",isAutheticated, getCourseByUser);
+courseRouter.put("/add-question",isAutheticated, addQuestion);
 
 export default courseRouter;
