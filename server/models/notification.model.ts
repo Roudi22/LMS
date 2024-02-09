8d895a19f48bda8f5cd1311e6d7b683768a01950
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-09 23:54:41 +0100

    create notification model

---
+++ server/models/notification.model.ts
@@ -0,0 +1,32 @@
+import e from "express";
+import mongoose, {Document, Model, Schema} from "mongoose";
+
+export interface INotification extends Document {
+    title: string;
+    message: string;
+    status: string;
+    userId: string;
+}
+
+const notificationSchema = new Schema<INotification>({
+    title: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'unread'
+    },
+    userId: {
+        type: String,
+        required: true
+    }
+},{timestamps: true}); // timestamps: true adds createdAt and updatedAt fields
+
+const NotificationModel: Model<INotification> = mongoose.model('Notification', notificationSchema);
+export default NotificationModel;
