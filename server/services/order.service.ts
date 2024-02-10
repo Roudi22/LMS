25eb205ce9fa12efcb93866469150580b99298a6
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-10 15:32:26 +0100

    change the logic

--- server/services/order.service.ts
+++ server/services/order.service.ts
@@ -1,8 +1,12 @@
-import { NextFunction } from "express";
+import { NextFunction, Response } from "express";
 import { catchAsyncErrors } from "../middleware/catchAsyncErrors";
 import OrderModel from "../models/order.model";
 // create new order
-export const newOrder = catchAsyncErrors(async (data:any, next: NextFunction) => {
+export const newOrder = catchAsyncErrors(async (data:any, res:Response) => {
     const order = await OrderModel.create(data);
-    next(order);
+    res.status(201).json({
+        success: true,
+        message: "Order created successfully",
+        order
+      });
 });
