950d477e73b49db459b69a5701b325459ece2ae5
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-11 22:10:20 +0100

    import get orders route

--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { newOrder } from "../services/order.service";
-import { isAutheticated } from "../middleware/auth";
-import { createOrder } from "../controllers/order.controller";
+import { authorizeRoles, isAutheticated } from "../middleware/auth";
+import { createOrder, getAllOrders } from "../controllers/order.controller";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/new-order",isAutheticated, createOrder);
+orderRouter.get("/get-all-orders",isAutheticated,authorizeRoles("admin"), getAllOrders);
 
 export default orderRouter;
