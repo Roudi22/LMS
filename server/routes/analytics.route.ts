5b08f8a270ad2620c90b4e70b36f092c9ff4624a
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-12 00:47:30 +0100

    add two new routes

--- server/routes/analytics.route.ts
+++ server/routes/analytics.route.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
-import { getUserDataAnalytics } from "../controllers/analytics.controller";
+import { getCoursesDataAnalytics, getOrdersDataAnalytics, getUserDataAnalytics } from "../controllers/analytics.controller";
 
 const analyticsRouter = express.Router();
 analyticsRouter.get("/get-user-data-analytics", isAutheticated, authorizeRoles("admin"), getUserDataAnalytics);
+analyticsRouter.get("/get-courses-data-analytics", isAutheticated, authorizeRoles("admin"), getCoursesDataAnalytics);
+analyticsRouter.get("/get-orders-data-analytics", isAutheticated, authorizeRoles("admin"), getOrdersDataAnalytics);
 export default analyticsRouter;
