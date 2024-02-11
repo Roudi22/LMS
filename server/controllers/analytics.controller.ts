6b20b9e40955debfb449bea9a14648ad0dde593e
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-12 00:48:15 +0100

    finish analytics controller

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -3,6 +3,8 @@ import ErrorHandler from "../utils/ErrorHandler";
 import { catchAsyncErrors } from "../middleware/catchAsyncErrors";
 import { generateLast12MothsData } from "../utils/analytics.generator";
 import userModel from "../models/user.model";
+import CourseModel from "../models/course.model";
+import OrderModel from "../models/order.model";
 
 // get user data analytics -- only for admin
 export const getUserDataAnalytics = catchAsyncErrors(
@@ -19,3 +21,33 @@ export const getUserDataAnalytics = catchAsyncErrors(
     }
   }
 );
+
+export const getCoursesDataAnalytics = catchAsyncErrors(
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+          const courses = await generateLast12MothsData(CourseModel);
+        res.status(200).json({
+          success: true,
+          message: "User data analytics",
+          courses
+        });
+      } catch (error:any) {
+        next(new ErrorHandler(500, error.message));
+      }
+    }
+  );
+
+export const getOrdersDataAnalytics = catchAsyncErrors(
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+          const orders = await generateLast12MothsData(OrderModel);
+        res.status(200).json({
+          success: true,
+          message: "User data analytics",
+          orders
+        });
+      } catch (error:any) {
+        next(new ErrorHandler(500, error.message));
+      }
+    }
+  );
