92788fec61bb9171ebb1bee99c9f09c6d260a398
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-12 17:51:10 +0100

    add get layout roúte

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
-import { createLayout, editLayout } from "../controllers/layout.controller";
+import { createLayout, editLayout, getLayout } from "../controllers/layout.controller";
 
 const layoutRouter = express.Router()
 
 layoutRouter.post("/create-layout", isAutheticated, authorizeRoles("admin"), createLayout);
 layoutRouter.put("/edit-layout", isAutheticated, authorizeRoles("admin"), editLayout);
+layoutRouter.get("/get-layout", getLayout);
 
 export default layoutRouter;
