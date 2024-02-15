314808a486c338a1aed969987558d0ff55770dfb
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-15 19:41:42 +0100

    delete a div

--- client/app/utils/NavItems.tsx
+++ client/app/utils/NavItems.tsx
@@ -49,19 +49,18 @@ const NavItems = ({activeItem, isMobile}: Props) => {
       </div>
       {isMobile && (
         <div className='800px:hidden mt-5'>
-          <div className='w-full text-center py-6'>
+          
               {
                 navItemsData && navItemsData.map((item, index) => (
                   <Link
               href={"/"} passHref key={item.id}>
-                <span className={`${ activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : "dark:text-white text-black"}`}>
+                <span className={`${ activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : "dark:text-white text-black"} block py-5 text-[18px] px-6 font-Poppins font-[400]`}>
                   {item.name}
                 </span>
               </Link>
                   
                 ))
               }
-          </div>
         </div>
       )}
     </>
