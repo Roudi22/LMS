5c96dff49aa56cc3eefbac4dd13cb148e3accba6
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-15 17:19:02 +0100

    fix layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,8 +1,19 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next";
+import { Poppins, Josefin_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./utils/theme-provider";
+import Navbar from "./components/Navbar";
+
+
+const poppins = Poppins({ 
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],  
+});
+const josefin = Josefin_Sans({ 
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],  
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +27,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${poppins.className} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}>
+      <ThemeProvider attribute="class" enableSystem defaultTheme="system">
+        <Navbar/>
+        {children}
+
+      </ThemeProvider>
+      </body>
     </html>
   );
 }
