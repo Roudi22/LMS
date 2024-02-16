220dde440f5f7c006433be24803aa00ffb07efc7
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-16 11:08:53 +0100

    create index file to export all the images needed

---
+++ client/app/assets/index.ts
@@ -0,0 +1,5 @@
+import client1 from "./client1.jpg"
+import client2 from "./client2.jpg"
+import client3 from "./client3.jpg"
+import heroImg from "./hero-svg.svg"
+export { client1, client2, client3, heroImg}
