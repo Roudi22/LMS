f39271b839464c64649e03877465a65943f3b821
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-16 21:50:53 +0100

    create verification component

---
+++ client/app/components/Auth/Verification.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useRef } from "react";
+import { toast } from "react-hot-toast";
+import { VscWorkspaceTrusted } from "react-icons/vsc";
+
+type Props = {
+  setRoute: (route: string) => void;
+};
+type VerifyNumber = {
+  "0": string;
+  "1": string;
+  "2": string;
+  "3": string;
+};
+const Verification = (props: Props) => {
+  const [invalidError, setInvalidError] = useState(false);
+  const inputRefs = [
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+  ];
+  const [verifyNumber, setVerifyNumber] = useState({
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+  } as VerifyNumber);
+  const verificationHandler = async () => {
+    setInvalidError(true);
+  };
+
+  const handleInputChange = (index: number, value: string) => {
+    setInvalidError(false);
+    const newVerifyNumber = { ...verifyNumber, [index]: value };
+    setVerifyNumber(newVerifyNumber);
+    if (value === "" && index > 0) {
+      inputRefs[index - 1].current?.focus();
+    } else if (value.length === 1 && index < 3) {
+      inputRefs[index + 1].current?.focus();
+    }
+  };
+  return (
+    <div>
+      <h1 className="title">Enter Verification Code</h1>
+      <div className="w-full flex items-center justify-center mt-2">
+        <div className="w-[80px] h-[80px] rounded-full bg-[#497df2] flex items-center justify-center">
+          <VscWorkspaceTrusted className="text-4xl text-white" size={40} />
+        </div>
+      </div>
+
+      <div className="mx-auto flex items-center justify-around my-6">
+        {Object.keys(verifyNumber).map((key, index) => (
+          <input
+          type="number"
+            ref={inputRefs[index]}
+            key={index}
+            value={verifyNumber[key as keyof VerifyNumber]}
+            onChange={(e) => handleInputChange(index, e.target.value)}
+            className={`w-[65px] h-[65px] bg-transparent border-[3px] rounded-[10px] flex items-center text-black dark:text-white justify-center text-[18px] font-Poppins outline-none text-center ${
+              invalidError
+                ? "shake border-red-500"
+                : "dark:border-white border-[#0000004a]"
+            }`}
+            placeholder=""
+            maxLength={1}
+          />
+        ))}
+      </div>
+      <div className="w-full flex justify-center">
+            <button className="button" onClick={verificationHandler}>
+                Verify OTP
+            </button>
+      </div>
+      <h5 className="text-center pt-4 font-Poppins text-[14px] text-black dark:text-white">
+            Go back to sign in? <span className="text-[#2190ff] pl-1 cursor-pointer" onClick={()=> props.setRoute("Login")}>Sign in</span>
+      </h5>
+    </div>
+  );
+};
+
+export default Verification;
