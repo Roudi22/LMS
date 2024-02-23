d8e0a71a36043bc228d84eacfb627b3295c2335a
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-23 14:36:56 +0100

    add dispatches when the app initialized

--- client/redux/store.ts
+++ client/redux/store.ts
@@ -11,3 +11,10 @@ export const store = configureStore({
     devTools: false,
 
 })
+
+// call the refresh token query to get a new access token
+const initializeApp = async () => {
+    await store.dispatch(apiSlice.endpoints.refreshToken.initiate({}, {forceRefetch: true}))
+    await store.dispatch(apiSlice.endpoints.loadUser.initiate({}, {forceRefetch: true})) // load user data from the server and update the state with the user's details if the user is logged in 
+}
+initializeApp()
