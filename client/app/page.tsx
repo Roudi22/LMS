164e6bedc7ae2c143340584ec35d5a951c3c18ba
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-23 14:37:52 +0100

    pass some states

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,19 +1,26 @@
-"use client"
-import React, {useState} from 'react'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
+"use client";
+import React, { useState } from "react";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
-type Props = {}
+type Props = {};
 
 const Page = (props: Props) => {
-  const [route, setRoute] = useState("Login")
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login");
   return (
     <div>
-      <Navbar route={route} setRoute={setRoute}/>
-      <Hero/>
+      <Navbar
+        route={route}
+        setRoute={setRoute}
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+      />
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
 export default Page;
-
