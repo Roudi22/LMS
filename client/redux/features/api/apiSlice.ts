d5a1b31a83eb69da570616591f66a93170db7735
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-23 14:36:17 +0100

    implement new queries

--- client/redux/features/api/apiSlice.ts
+++ client/redux/features/api/apiSlice.ts
@@ -1,9 +1,38 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { userLoggedIn } from "../auth/authSlice";
 
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/v1/" }), // process.env.NEXT_PUBLIC_API_URI is the base url for the api server
-    endpoints: (builder) => ({}),
+    endpoints: (builder) => ({
+        refreshToken: builder.query({
+            query: (data) => ({
+                url: "refreshtoken",
+                method: "GET",
+                credentials: "include" as const,
+            }),
+        }),
+        loadUser: builder.query({
+            query: (data) => ({
+                url: "me",
+                method: "GET",
+                credentials: "include" as const,
+            }),
+            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+                try {
+                  const result = await queryFulfilled;
+                  dispatch(
+                    userLoggedIn({
+                      accessToken: result.data.accessToken,
+                      user: result.data.user
+                    })
+                  );
+                } catch (error: any) {
+                  console.log("error in load user",error);
+                }
+              }
+        }),
+    }),
 });
 
-export const {} = apiSlice;
+export const {useRefreshTokenQuery, useLoadUserQuery} = apiSlice;
