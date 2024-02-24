9d16ac79995b45521152bd6143eb3fbfd9e709e4
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-24 18:29:40 +0100

    add logout query

--- client/redux/features/auth/authApi.ts
+++ client/redux/features/auth/authApi.ts
@@ -1,5 +1,5 @@
 import { apiSlice } from "../api/apiSlice";
-import { userLoggedIn, userRegistration } from "./authSlice";
+import { userLoggedIn, userLoggedOut, userRegistration } from "./authSlice";
 
 type RegistrationResponse = {
     message: string;
@@ -93,8 +93,26 @@ export const authApi = apiSlice.injectEndpoints({
                 console.log("error from login mutation",error);
               }
             }
-          })
+          }),
+          logOut: builder.query({
+            query: () => ({
+              url: "logout-user",
+              method: "GET",
+              credentials : "include" as const
+            }),
+            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+              try {
+                dispatch(
+                  userLoggedOut() // dispatching the userLoggedOut action
+                );
+                console.log("logged out successfully from authApi");
+              } catch (error: any) {
+                console.log("error from log out mutation",error);
+              }
+            }
+          }),
+
     })
 });
 
-export const { useRegisterMutation, useActivationMutation, useLoginMutation, useSocialAuthMutation} = authApi;
+export const { useRegisterMutation, useActivationMutation, useLoginMutation, useSocialAuthMutation, useLogOutQuery} = authApi;
