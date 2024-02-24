7410501d1d269bf4b24f550c9a849ab5a727782b
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-24 12:35:22 +0100

    create profile page to show the profile on route /profile

---
+++ client/app/profile/page.tsx
@@ -0,0 +1,37 @@
+"use client"
+import React, { useState } from 'react'
+import Protected from '../hooks/useProtected'
+import Navbar from '../components/Navbar'
+import Profile from '../components/Profile/Profile'
+import Heading from '../utils/Heading'
+import { useSelector } from 'react-redux'
+
+type Props = {}
+
+const Page = (props: Props) => {
+    const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login");
+  const {user} = useSelector((state:any) => state.auth);
+  return (
+    <div>
+        <Protected>
+        <Heading
+      title={`${user.name} Profile - LMS - Endless Dev`}
+      description="LMS is a platform for students to learn and get help from teachers"
+      keywords="Prograaming,MERN,Redux,Machine Learning"
+      />
+        <Navbar
+        route={route}
+        setRoute={setRoute}
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+      />
+      <Profile/>
+        </Protected>
+    </div>
+  )
+}
+
+export default Page
