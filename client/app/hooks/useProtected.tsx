1c87c7147acf68bd050b81eb10ee935e6f9a2a29
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-24 12:36:52 +0100

    create a component to protect the profile page from accessing if the user is not logged in

---
+++ client/app/hooks/useProtected.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { redirect } from "next/navigation";
+import UserAuth from "./userAuth";
+
+interface Props {
+    children: React.ReactNode;
+
+}
+export default function Protected({children}:Props){
+    const isAuthenticated = UserAuth();
+    return isAuthenticated ? children : redirect("/");
+}
