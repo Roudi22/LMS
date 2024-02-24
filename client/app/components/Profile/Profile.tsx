89cba13a45d0904bd2051b87f048c544da27f672
Roudi22 <roudi.alzkemi@gmail.com>
2024-02-24 12:37:25 +0100

    create the profile component

---
+++ client/app/components/Profile/Profile.tsx
@@ -0,0 +1,40 @@
+"use client"
+import React, { useState } from 'react'
+import SideBarProfile from './SideBarProfile';
+
+type Props = {}
+
+const Profile = (props: Props) => {
+    const [scroll, setScroll] = useState(false);
+    const [avatar, setAvatar] = useState(null);
+    const [active, setActive] = useState(1);
+    const logoutHandler = () => {
+      console.log("logout");
+    }
+    
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+          setScroll(true);
+        } else {
+          setScroll(false);
+        }
+      });
+    }
+  return (
+    <div className='w-[85%] flex mx-auto'>
+        <div className={`w-[60px] 800px:w-[310px] h-[450px] dark:bg-slate-900 bg-opacity-90 border bg-white dark:border-[#ffffff1d] border-[#00000014] rounded-[5px] shadow-sm dark:shadow-sm mt-[80px] mb-[80px] sticky ${
+          scroll ? "top-[120px]" : "top-[30px]"
+        } left-[30px]`}>
+          <SideBarProfile 
+          active={active}
+          avatar={avatar}
+          setActive={setActive}
+          logoutHandler={logoutHandler}
+          />
+        </div>
+    </div>
+  )
+}
+
+export default Profile
