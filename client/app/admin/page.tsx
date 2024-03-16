03055bd3b132edd9a41ba036e7d359c28d9e21ad
Roudi22 <roudi.alzkemi@gmail.com>
2024-03-16 16:48:13 +0100

    dashboard component

--- client/app/admin/page.tsx
+++ client/app/admin/page.tsx
@@ -1,10 +1,28 @@
+"use client"
+import AdminSidebar from '../components/admin/sidebar/AdminSidebar'
 import React from 'react'
+import Heading from '../utils/Heading'
+import DashboardHero from '../components/admin/DashboardHero'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>Admin Dashboard</div>
+    <div>
+      <Heading
+      title='admin dashboard'
+      description='This is the admin dashboard'
+      keywords='admin, dashboard'
+      />
+      <div className='flex h-[200vh]'>
+        <div className='1500px:w-[16%] w-1/5'>
+          <AdminSidebar />
+        </div>
+        <div className='w-[85%]'>
+          <DashboardHero />
+        </div>
+      </div>
+    </div>
   )
 }
 
